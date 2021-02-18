74f31e6ac91964211a8326d46db228242d3c2ade
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-02-17 22:54:33 -0500

    Create restaurant

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: 0..5}
-  # validates :rating, numericality: { only_integer: true}
+  #  validates :rating, numericality: { only_integer: true}
   # }
+  validates :rating, presence: true, inclusion: { in: 0..5 }
+ 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning the database...'
+  Restaurant.destroy_all
+puts 'database is clean'
+
+puts 'creating restaurants'
+10.times do 
+    restaurant = Restaurant.create(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.street_address,
+        phone_number: Faker::PhoneNumber.cell_phone,
+        category: %w(chinese italian japanese french belgian)
+    )
+    puts "restaurant#{restaurant.id} is created" 
+end
+puts 'done'
+
