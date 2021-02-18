f19b68d9d9db5f5b4585049ded8c03516b953a9d
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-02-18 08:14:05 -0500

    Add forms for creating a new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,8 @@ class RestaurantsController < ApplicationController
     def index 
        @restaurants = Restaurant.all
     end
+
+    def new 
+       @restaurant = Restaurant.new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
   get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
 end
