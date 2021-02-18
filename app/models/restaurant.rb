1056f6b11e1e2e9172117d3d9c215e409da8fbff
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-02-17 20:24:39 -0500

    Add Validation to review and restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,7 @@
 class Restaurant < ApplicationRecord
-    
+    has_many :reviews, dependent: :destroy
     validates :name, presence: true
     validates :address, presence: true
     validates :category, presence: true,
-        inclusion: { in: %w(chinese italian japanese french belgian), 
-        message: "#{category} is not a valid choose"}
+        inclusion: { in: %w(chinese italian japanese french belgian)}
 end
